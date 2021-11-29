d0938c45b695659d15b2ec9983b44edc80fb133f
Chengzong <chengzongjiang@outlook.com>
2021-11-29 16:16:21 -0500

    database arn fasta

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
-    public void bruh(){
+    public static ArrayList<String> readFasta() throws FileNotFoundException {
 
-    }
-    public static void main(String[] args) {
-
+        ArrayList<String> database = new ArrayList<String>();
+        try (Scanner sc = new Scanner(new File("src/tRNAs.fasta"))) {
+            while (sc.hasNextLine()) {
+                ArrayList<String> line = new ArrayList<String>();
+                database.add(sc.nextLine().trim());
+            }
+        }
+        return database;
+    };
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(readFasta());
     }
 }
