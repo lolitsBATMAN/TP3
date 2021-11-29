2eb6994327855360ca069cd509caafc125168710
Chengzong <chengzongjiang@outlook.com>
2021-11-29 18:36:56 -0500

    KMER IMPLEMENTED

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,39 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
-    public static ArrayList<String> readFasta() throws FileNotFoundException {
+
+    // 1.1
+    public static ArrayList<String> readFasta(String x) throws FileNotFoundException {
 
         ArrayList<String> database = new ArrayList<String>();
-        try (Scanner sc = new Scanner(new File("src/tRNAs.fasta"))) {
+        try (Scanner sc = new Scanner(new File(x))) {
             while (sc.hasNextLine()) {
-                ArrayList<String> line = new ArrayList<String>();
                 database.add(sc.nextLine().trim());
             }
         }
         return database;
-    };
+    }
+
+    //1.2
+    public static ArrayList<String> kmer (String word, int k){
+        ArrayList<String> mer = new ArrayList<String>();
+
+        for (int i  = 0; i <= word.length() - k ; i++){
+            mer.add(word.substring(i, i+k));
+        }
+
+        return mer;
+    }
+
+    //1.3
+    //1.4
+    //1.5
+    //1.6
+    //1.7
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println(readFasta());
+        System.out.println(readFasta("src/tRNAs.fasta"));
+        ArrayList<String> database = readFasta("src/tRNAs.fasta");
+        ArrayList<String> unknown = readFasta("src/unknown.fasta");
+        System.out.println(kmer(unknown.get(1), 11));
     }
 }
