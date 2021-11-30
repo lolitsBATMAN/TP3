0f78f4bbeaa18141a707851418514454671cc887
Ahmad El-Rahim <ahmad_56@live.ca>
2021-11-29 22:06:12 -0500

    database structure

---
+++ src/DBentry.java
@@ -0,0 +1,14 @@
+/**
+ * Type of entry of the databse
+ */
+public class DBentry {
+    //jai mi la sequence dans 1 une seule string, mais on verra apres si sa serait mieux des la mettre dans une
+    //arraylist caractere par caracteres
+    String sequenceInfo;
+    String sequence;
+
+    public DBentry(String sequenceInfo, String sequence){
+        this.sequenceInfo = sequenceInfo;
+        this.sequence = sequence;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,31 @@ import java.util.ArrayList;
 
 public class Main {
 
+
     // 1.1
     //je m'en criss un peu ici so j'ai tout criss dans un tableau, dans le fond
     //les index 0,2,4,6... sont les >dsad|dasd|sdads| et les index 1,3,5,... sont
     //les nucleotides, good luck
     //trop de trouble de les filtrer
-    public static ArrayList<String> readFasta(String x) throws FileNotFoundException {
 
-        ArrayList<String> database = new ArrayList<String>();
-        try (Scanner sc = new Scanner(new File(x))) {
+    //2e argument la dabatase quon creer dans la main
+    public static void readFasta(String file, ArrayList<DBentry> database) throws FileNotFoundException {
+
+
+        try (Scanner sc = new Scanner(new File(file))) {
+
             while (sc.hasNextLine()) {
-                database.add(sc.nextLine().trim());
+                String infos = sc.nextLine();
+                String sequence = sc.nextLine();
+
+                //create entry that contains these 2 strings
+                DBentry currEntry = new DBentry(infos,sequence);
+
+                //add entry to database
+                database.add(currEntry);
+
             }
         }
-        return database;
     }
 
     //1.2
@@ -38,9 +49,14 @@ public class Main {
     //1.6
     //1.7
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println(readFasta("src/tRNAs.fasta"));
-        ArrayList<String> database = readFasta("src/tRNAs.fasta");
-        ArrayList<String> unknown = readFasta("src/unknown.fasta");
-        System.out.println(kmer(unknown.get(1), 11));
+        //System.out.println(readFasta("src/tRNAs.fasta"));
+        //ArrayList<DBentry> database = readFasta("src/tRNAs.fasta");
+        //ArrayList<String> unknown = readFasta("src/unknown.fasta");
+        //System.out.println(kmer(unknown.get(1), 11));
+
+
+        //Creer ici la database pour avoir acces partout
+        ArrayList<DBentry> database = new ArrayList<>();
+        readFasta("C:\\Users\\Admin\\Documents\\GitHub\\TP3\\src\\tRNAs.fasta", database);
     }
 }
